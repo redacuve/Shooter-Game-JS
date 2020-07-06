9c98a732ff0a097681630b5d4c0576874b2626ca
Rey David Cuevas <cuevas.vela@gmail.com>
2020-07-06 11:16:22 -0500

    added movment to the player sprite with up down left and right keys

--- src/scenes/sceneMain.js
+++ src/scenes/sceneMain.js
@@ -26,9 +26,33 @@ class SceneMain extends Phaser.Scene {
       this,
       this.game.config.width * 0.5,
       this.game.config.height * 0.5,
-      'sprPlayerP50D',
-      'sprPlayerP50D'
+      "sprPlayerP50D",
+      "sprPlayerP50D"
     );
+
+    this.keyUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    this.keyDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+    this.keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    this.keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    this.keySpace = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
+  }
+
+  update() {
+    this.player.update();
+
+    if (this.keyUp.isDown) {
+      this.player.moveUp();
+    } else if (this.keyDown.isDown) {
+      this.player.moveDown();
+    }
+
+    if (this.keyLeft.isDown) {
+      this.player.moveLeft();
+    } else if (this.keyRight.isDown) {
+      this.player.moveRight();
+    }
   }
 }
 
